f62f5aab119df7823595768d8f82f839e73a0636
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-09-07 18:34:38 +0800

    update: add footer

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -63,6 +63,12 @@ const HomeLayout = ({ children }) => {
           {children}
         </Col>
       </Row>
+      <footer className="flex-center text-secondary py-3">
+        <div className="d-flex align-items-center small">
+          <span className="fw-bold me-2">SQL Adventure</span>
+          <span className="opacity-75">© 2024 PDWM Pilkom</span>
+        </div>
+      </footer>
     </Container>
   );
 };
--- src/pages/admin/_root.jsx
+++ src/pages/admin/_root.jsx
@@ -29,6 +29,12 @@ const AdminRootPage = () => {
           <Outlet />
         </Col>
       </Row>
+      <footer className="flex-center text-dark py-3 border-top">
+        <div className="d-flex align-items-center small">
+          <span className="fw-bold me-2">SQL Adventure</span>
+          <span className="opacity-75">© 2024 PDWM Pilkom</span>
+        </div>
+      </footer>
     </>
   );
 };
