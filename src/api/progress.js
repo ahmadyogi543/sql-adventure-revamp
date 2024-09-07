738edabe2e3f82793b0a1d14a827af759016668b
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-09-07 20:51:38 +0800

    update: add stage data

--- src/api/progress.js
+++ src/api/progress.js
@@ -13,3 +13,17 @@ export async function getAllUsersProgress(token) {
   }
   return result.data;
 }
+
+export async function getStageData(token, id) {
+  const response = await fetch(`${config.API_URL}/users/stages/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const result = await response.json();
+  if (result.status === "failed") {
+    throw new Error(result.message);
+  }
+  return result.data.stage_data;
+}
--- src/menus/StageMenu.jsx
+++ src/menus/StageMenu.jsx
@@ -2,104 +2,50 @@ import { Col, Container, Row } from "react-bootstrap";
 
 import { useMenuContext } from "../context/MenuContext";
 import GameplayMenuLayout from "../layouts/GameplayMenuLayout";
-
-const DATA = [
-  {
-    id: 1,
-    unlock: true,
-    uri: "/images/stage-cards/1.png",
-    star: 2,
-  },
-  {
-    id: 2,
-    unlock: true,
-    uri: "/images/stage-cards/2.png",
-    star: 3,
-  },
-  {
-    id: 3,
-    unlock: true,
-    uri: "/images/stage-cards/3.png",
-    star: 2,
-  },
-  {
-    id: 4,
-    unlock: true,
-    uri: "/images/stage-cards/4.png",
-    star: 0,
-  },
-  {
-    id: 5,
-    unlock: false,
-    uri: "/images/stage-cards/unknown.png",
-    star: 0,
-  },
-  {
-    id: 6,
-    unlock: false,
-    uri: "/images/stage-cards/unknown.png",
-    star: 0,
-  },
-  {
-    id: 7,
-    unlock: false,
-    uri: "/images/stage-cards/unknown.png",
-    star: 0,
-  },
-  {
-    id: 8,
-    unlock: false,
-    uri: "/images/stage-cards/unknown.png",
-    star: 0,
-  },
-  {
-    id: 9,
-    unlock: false,
-    uri: "/images/stage-cards/unknown.png",
-    star: 0,
-  },
-  {
-    id: 10,
-    unlock: false,
-    uri: "/images/stage-cards/unknown.png",
-    star: 0,
-  },
-];
+import { useAuthContext } from "../context/AuthContext";
+import { useEffect, useState } from "react";
+import { getStageData } from "../api/progress";
 
 const StageMenu = () => {
+  const { token, user } = useAuthContext();
   const { toGameplayMenu } = useMenuContext();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getStageData(token, user.id)
+      .then((data) => setData(data))
+      .catch((err) => {
+        alert("Kesalahan: terjadi gangguan pada sistem!");
+        console.error(err);
+      });
+  }, []);
 
   return (
     <GameplayMenuLayout title="PILIH TOPIK">
       <Container className="p-5">
         <Row className="g-4 gap-2">
-          {DATA.map((data) => (
-            <Col
-              className="flex-center flex-column"
-              key={`stage-menu-${data.id}`}
-            >
+          {data.map((d) => (
+            <Col className="flex-center flex-column" key={`stage-menu-${d.id}`}>
               <img
-                onClick={() => toGameplayMenu({ data })}
+                onClick={() => toGameplayMenu({ data: d })}
                 className={`d-block rounded ${
-                  data.unlock
+                  d.unlock
                     ? "cursor-pointer animation-shake"
                     : "cursor-not-allowed"
                 }`}
                 width={160}
-                src={data.uri}
+                src={d.uri}
                 alt=""
               />
               <div className="mt-2 flex-center">
-                {Array.from({ length: data.star }, (_, k) => k + 1).map(
-                  (_, k) => (
-                    <img
-                      key={`star-card-${k}`}
-                      width={24}
-                      src="/images/stage-cards/star.png"
-                      alt=""
-                    />
-                  )
-                )}
+                {Array.from({ length: d.star }, (_, k) => k + 1).map((_, k) => (
+                  <img
+                    key={`star-card-${k}`}
+                    width={24}
+                    src="/images/stage-cards/star.png"
+                    alt=""
+                  />
+                ))}
               </div>
             </Col>
           ))}
